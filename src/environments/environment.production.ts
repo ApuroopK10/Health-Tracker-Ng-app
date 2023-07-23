e436482f1b2e0f6aaa2b1789de60fc1ae91ddccb
ltr <akambampati@hcf.com.au>
2023-07-23 18:44:20 +0530

    Added env., and assets

---
+++ src/environments/environment.production.ts
@@ -0,0 +1,12 @@
+export const environment = {
+  production: true,
+  firebase: {
+    projectId: 'fitness-tracker-f8ba4',
+    appId: '1:584084332328:web:04230c8971d00681983a1d',
+    storageBucket: 'fitness-tracker-f8ba4.appspot.com',
+    apiKey: 'AIzaSyCT-7xUG1U_wrwiSpto_ACojU2p_H76q8k',
+    authDomain: 'fitness-tracker-f8ba4.firebaseapp.com',
+    messagingSenderId: '584084332328',
+    measurementId: 'G-YMW7EJLV7C',
+  },
+};
